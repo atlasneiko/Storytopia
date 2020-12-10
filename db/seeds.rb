94a64a43b7bb00ecb20e6c79ddbceb31518e6687
Michael Chen <atlasneiko@gmail.com>
2020-12-09 18:11:03 -0800

    seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,185 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 User.create(username: 'demo', password:"demoOne2", email:"atlasneiko@gmail.com")
+users = User.create([
+  {
+    username: "JaylynGalloway",
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: "RyleighChurch",
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: "RaquelHanson",
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: "PeytonManning",
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: "KatelynnOconnor",
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: "ErikaKeller",
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: "WyattVelez",
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+  {
+    username: ,
+    password: ,
+    email: ,
+    about: 
+  },
+])
--- frontend/components/greeting/greeting.jsx
+++ frontend/components/greeting/greeting.jsx
@@ -3,16 +3,29 @@ import { Link, useLocation } from "react-router-dom";
 
 export default ({ currentUser, logout, loggedIn }) => {
 	const location = useLocation();
+	const greeting = () => {
+		let date = new Date();
+		let hour = date.getHours();
+		if (hour >= 5 && hour <= 12) {
+			return "Good morning, ";
+		} else if (hour > 12 && hour < 18) {
+			return "Good afternoon, ";
+		} else {
+			return "Good evening, ";
+		}
+	};
 	const login = (
 		<Link to="/login" id="login">
 			Login
 		</Link>
 	);
+
 	const signup = (
 		<Link to="/signup" id="signup">
 			Sign Up
 		</Link>
 	);
+
 	const links = () => {
 		if (location.pathname === "/login") {
 			return <nav className="nav-links">{signup}</nav>;
@@ -31,7 +44,7 @@ export default ({ currentUser, logout, loggedIn }) => {
 	const message = () => (
 		<div className="message">
 			<h2>
-				Hello,
+				{greeting()}
 				<Link to={`/users/${currentUser.id}`}>{currentUser.username}</Link>
 			</h2>
 			<button onClick={logout} id="logout">
@@ -39,5 +52,6 @@ export default ({ currentUser, logout, loggedIn }) => {
 			</button>
 		</div>
 	);
+
 	return loggedIn ? message() : links();
 };
