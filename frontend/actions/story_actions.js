b6383217cd8c54cc03ca59f901a2d1514f97c04e
Michael Chen <atlasneiko@gmail.com>
2020-12-10 15:44:15 -0800

    make story actions

---
+++ frontend/actions/story_actions.js
@@ -0,0 +1,66 @@
+import * as StoryAPI from '../util/story_api_util'
+
+export const RECEIVE_ALL_STORIES = 'RECEIVE_ALL_STORIES';
+export const RECEIVE_STORY = 'RECEIVE_STORY';
+export const RECEIVE_STORY_ERRORS = 'RECEIVE_STORY_ERRORS';
+export const DELETE_STORY = "DELETE_STORY"
+
+
+const receiveAllStories = stories => ({
+  type: RECEIVE_ALL_STORIES,
+  stories
+})
+
+const receiveErrors = errors => ({
+  type: RECEIVE_USER_ERRORS,
+  errors
+})
+
+const receiveStory = story => ({
+  type: RECEIVE_STORY,
+  story
+})
+
+const deleteStory = storyId => ({
+  type: DELETE_STORY,
+  storyId
+})
+
+export const fetchAllStories = () => dispatch => (
+  StoryAPI.fetchAllStories
+    .then(
+      stories => dispatch(receiveAllStories(stories)),
+      err => dispatch(reeciveErrors(err.responseJSON))
+    )
+)
+
+export const fetchStory = storyId => dispatch => (
+  StoryAPI.fetchStory
+    .then(
+      story => dispatch(receiveStory(story)),
+      err => dispatch(reeciveErrors(err.responseJSON))
+    )
+)
+
+export const postStory = story => dispatch => (
+  StoryAPI.postStory(story)
+    .then(
+      story => dispatch(receiveStory(story)),
+      err => dispatch(receiveErrors(err.responseJSON))
+    )
+)
+
+export const editStory = story => dispatch => (
+  StoryAPI.editStory(story)
+    .then(
+      story => dispatch(receiveStory(story)),
+      err => dispatch(reeciveErrors(err.responseJSON))
+    )
+)
+export const removeStory = storyId => dispatch => (
+  StoryAPI.deleteStory
+    .then(
+      () => dispatch(deleteStory(storyId)),
+      err => dispatch(reeciveErrors(err.responseJSON))
+    )
+)
