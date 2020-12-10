92ee6cf73c344935d73c3c332e9bd9e50dd81574
Michael Chen <atlasneiko@gmail.com>
2020-12-10 14:09:54 -0800

    redo story table and model

---
+++ app/models/story.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  subtitle   :string           not null
+#  user_id    :integer          not null
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Story < ApplicationRecord
+  validates :title, :subtitle, :user_id, :body, presence: true
+  validates [:title, :subtitle], uniqueness: true
+
+  belongs_to :user
+end
---
+++ test/models/story_test.rb
@@ -0,0 +1,19 @@
+# == Schema Information
+#
+# Table name: stories
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  subtitle   :string           not null
+#  user_id    :integer          not null
+#  body       :text             not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+require 'test_helper'
+
+class StoryTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
