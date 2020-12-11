7a845b568fe54d544324c45dd659ab5d224c8f0a
Michael Chen <atlasneiko@gmail.com>
2020-12-10 20:43:10 -0800

    remove sotries

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  namespace :api do
-    get 'sotriesrails/ckl'
-  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'static_pages#root'
   
