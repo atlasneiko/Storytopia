4eee6090e5892842fd3e9f72a314a912a762f8ad
Michael Chen <atlasneiko@gmail.com>
2020-12-10 20:08:01 -0800

    fix the freaking thing...

--- app/models/story.rb
+++ app/models/story.rb
@@ -10,17 +10,7 @@
 #  created_at :datetime         not null
 #  updated_at :datetime         not null
 #
-class Story < ApplicationRecord
-  validates :title, :subtitle, :user_id, :body, presence: true
 
-  belongs_to :user
-=======
-#  id       :bigint           not null, primary key
-#  user_id  :integer          not null
-#  title    :string           not null
-#  subtitle :string
-#  body     :text             not null
-#
 class Story < ApplicationRecord
   validates :title, :body, :user_id, presence: true
   belongs_to :user
