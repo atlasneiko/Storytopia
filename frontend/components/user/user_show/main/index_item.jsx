82d0e8c14c93ed10a4049a45e75f9f1bf2c4f4de
Michael Chen <atlasneiko@gmail.com>
2020-12-11 16:38:32 -0800

    add body to story index item

--- frontend/components/user/user_show/main/index_item.jsx
+++ frontend/components/user/user_show/main/index_item.jsx
@@ -13,16 +13,25 @@ class IndexItem extends React.Component {
 		const { story } = this.props;
 		if (story !== undefined) {
 			console.log(story);
+
 			let date = story.updatedAt.slice(0, 10).split("-");
 			[date[0], date[1], date[2]] = [date[1], date[2], date[0]];
 			date = date.join("/");
-			console.log(date);
+
+			let body = story.body.split("<br />").map((paragraph, i) => (
+				<p key={`${story.id}-${i}`}>
+					{paragraph}
+					<br />
+				</p>
+			));
+
+			console.log(body);
 			return (
 				<li>
 					<h3>{story.title}</h3>
 					<h4>{story.subtitle}</h4>
-					<p>{story.updateAt}</p>
 					<p>{date}</p>
+					<div>{body}</div>
 				</li>
 			);
 		} else {
