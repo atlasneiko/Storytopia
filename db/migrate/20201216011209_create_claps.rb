5ab3f26c668ee8fabb528e995a30300e6dd4993c
Michael Chen <atlasneiko@gmail.com>
2020-12-15 17:14:54 -0800

    add claps table

---
+++ db/migrate/20201216011209_create_claps.rb
@@ -0,0 +1,13 @@
+class CreateClaps < ActiveRecord::Migration[5.2]
+  def change
+    create_table :claps do |t|
+      t.integer :user_id, null: false
+      t.integer :story_id, null: false
+      t.integer :clap_count, null: false, default: 1
+      t.timestamps
+    end
+    add_index :claps, :user_id
+    add_index :claps, :story_id
+    add_index :claps, [:user_id, :story_id], unique: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_12_15_184437) do
+ActiveRecord::Schema.define(version: 2020_12_16_011209) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "claps", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "story_id", null: false
+    t.integer "clap_count", default: 1, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["story_id"], name: "index_claps_on_story_id"
+    t.index ["user_id", "story_id"], name: "index_claps_on_user_id_and_story_id", unique: true
+    t.index ["user_id"], name: "index_claps_on_user_id"
+  end
 
   create_table "stories", force: :cascade do |t|
     t.string "title", null: false
