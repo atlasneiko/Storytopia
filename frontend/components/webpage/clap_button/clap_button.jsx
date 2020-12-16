f865d97eac5cccac0be35951eb78784c3f04e2c2
Michael Chen <atlasneiko@gmail.com>
2020-12-16 09:27:44 -0800

    add clap button container

--- frontend/components/webpage/clap_button/clap_button.jsx
+++ frontend/components/webpage/clap_button/clap_button.jsx
@@ -6,4 +6,16 @@ import {
 	updateClap,
 } from "../../../actions/clap_actions";
 
+const mSTP = (state, ownProps) => ({
+	user: state.entities.users[state.session.currentUser.id],
+	story: ownProps.story,
+	claps: state.entities.claps,
+});
 
+const mDTP = (dispatch) => ({
+	eraseClapErrors: () => dispatch(eraseClapErrors()),
+	createClap: (clap) => dispatch(createClap(clap)),
+	updateClap: (clap) => dispatch(updateClap(clap)),
+});
+
+export default connect(mSTP, mDTP)();
