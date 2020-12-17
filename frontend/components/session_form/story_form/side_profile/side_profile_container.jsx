f9fea078f11cf2039960b3f30c15fb6e821653a1
Michael Chen <atlasneiko@gmail.com>
2020-12-17 11:15:52 -0800

    remove duplicated files

--- frontend/components/session_form/story_form/side_profile/side_profile_container.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { connect } from "react-redux";
-import sideProfile from "./side_profile";
-const mSTP = (state, ownProps) => ({});
-
-const mDTP = (dispatch) => ({});
-
-export default connect(mSTP, mDTP)(sideProfile);
