904b21d7d1cc39d143dc8e0e6ca372ecf6f0abd1
Michael Chen <atlasneiko@gmail.com>
2020-12-17 04:57:45 -0800

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -22,13 +22,13 @@ class User < ApplicationRecord
   has_many :stories
   has_many :claps
 
-  has_many :followees, 
-    foreign_key: followee_id, 
-    class_name: :Following
+  # has_many :followees, 
+  #   foreign_key: followee_id, 
+  #   class_name: :Following
 
-  has_many :followers, 
-    foreign_key: :follower_id, 
-    class_name: Following
+  # has_many :followers, 
+  #   foreign_key: :follower_id, 
+  #   class_name: Following
 
   attr_reader :password
   
